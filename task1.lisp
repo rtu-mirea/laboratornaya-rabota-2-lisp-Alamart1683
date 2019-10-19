06d8005bda3dfbb5782ec227b50b0fd1f3e7c43f
Alamart1683 <Alamart1683@yandex.ru>
2019-10-19 10:11:26 +0300

    added task 1.2

--- task1.lisp
+++ task1.lisp
@@ -1,4 +1,4 @@
-;; insert
+;; insert task 1.1
 (defun insert (value list index)
   (cond ((= index 0) (push value list))
         ((> index (length list)) nil)
@@ -9,3 +9,5 @@
 
 (insert 228 (list 14 88 22 17 32 84 56 22) 2)
 
+;; remove task 1.2
+(remove 17 (list 14 88 22 17 32 84 56 22))
